c84e3f764bc9102b6fbbcb70fdae3c876adb75a2
Mely <meliusanorahariyanti@gmail.com>
2024-09-17 10:25:43 +0700

    change bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
         primary: "#e07a5f",
         secondary: "#81b29a",
         dark: "#3d405b",
+        light_yellow: "#f4f1de",
+        dark_orange: "#e07a5f",
       },
       screens: {
         "2xl": "1320px",
